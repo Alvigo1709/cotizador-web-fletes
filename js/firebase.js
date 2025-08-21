01ddd95685c4bb8388fce63599b97aac634f874e
Alvigo1709 <127364982+Alvigo1709@users.noreply.github.com>
2025-08-20 23:09:59 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,11 @@
 // firebase.js
+// Configuración e inicialización de Firebase
+
+// Importación de módulos necesarios
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
-// Configuración de tu proyecto
+// Configuración del proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBYMXyBtPMHSDfsjnGR_xZ4kbAWiZjicy8",
   authDomain: "cotizador-fletes-ca0d8.firebaseapp.com",
@@ -12,8 +15,6 @@ const firebaseConfig = {
   appId: "1:385602786001:web:8b09f79ef239b062622bf0"
 };
 
-// Inicializa Firebase
+// Inicializa Firebase y Firestore
 export const app = initializeApp(firebaseConfig);
-
-// Inicializa Firestore
 export const db = getFirestore(app);
