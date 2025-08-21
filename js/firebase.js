3df60d688e84acc289800e2d65628dc921e7f53d
Alvigo1709 <127364982+Alvigo1709@users.noreply.github.com>
2025-08-20 23:04:01 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,19 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
+// Configuración de tu proyecto
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "cotizador-fletes",
-  storageBucket: "cotizador-fletes.appspot.com",
-  messagingSenderId: "ID_DEL_MENSAJERO",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBYMXyBtPMHSDfsjnGR_xZ4kbAWiZjicy8",
+  authDomain: "cotizador-fletes-ca0d8.firebaseapp.com",
+  projectId: "cotizador-fletes-ca0d8",
+  storageBucket: "cotizador-fletes-ca0d8.firebasestorage.app",
+  messagingSenderId: "385602786001",
+  appId: "1:385602786001:web:8b09f79ef239b062622bf0"
 };
 
+// Inicializa Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Inicializa Firestore
 export const db = getFirestore(app);
